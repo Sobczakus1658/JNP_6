b2f2a60faf82989794261e57f8c3763bcd8ad8ac
Paweł Preibisch <pawka1114@gmail.com>
2023-01-14 16:48:43 +0100

    Debug

--- worldcup2022.h
+++ worldcup2022.h
@@ -55,6 +55,10 @@ class Player {
             state = fine;
         }
 
+        std::string getName() const {
+            return name;
+        }
+
         void setBankrupt() {
             isBankrupt = true;
         }
@@ -117,7 +121,7 @@ class Match : public virtual Field {
 
 class SeasonBegin : public virtual Field {
     private:
-        size_t passBonus = 50;
+        size_t passBonus;
     public:
         SeasonBegin(std::string const &name, size_t pB):
                 Field(name),
@@ -175,7 +179,7 @@ class YellowCard : public virtual Field {
                 suspension(susp) {}
 
         void passingAction(Player * player) override {
-            //nic się nie dzieje
+            // nic się nie dzieje
         }
 
         void landingAction(Player * player) override {
@@ -228,13 +232,14 @@ class Board {
             fields.push_back(std::make_shared<SeasonBegin>("początek sezonu", 50));
             fields.push_back(std::make_shared<Match>("mecz z San Marino", 1, 160));
             fields.push_back(std::make_shared<RestDay>("dzień wolny od treningu"));
-            fields.push_back(std::make_shared<Match>("mecz z Lichtenstainem", 1, 220));
+            fields.push_back(std::make_shared<Match>("mecz z Liechtensteinem", 1, 220));
             fields.push_back(std::make_shared<YellowCard>("żółta kartka", 3));
             fields.push_back(std::make_shared<Match>("mecz z Meksykiem", 2.5, 300));
             fields.push_back(std::make_shared<Match>("mecz z Arabią Saudyjską", 2.5, 280));
             fields.push_back(std::make_shared<Bookmaker>("bukmacher", 100, 100));
-            fields.push_back(std::make_shared<Match>("mecz z Argentyną", 2.5, 280));
+            fields.push_back(std::make_shared<Match>("mecz z Argentyną", 2.5, 250));
             fields.push_back(std::make_shared<Goal>("gol", 120));
+            fields.push_back(std::make_shared<Match>("mecz z Francją", 5, 400));
             fields.push_back(std::make_shared<Penalty>("rzut karny", 180));
 
         }
@@ -253,6 +258,7 @@ class WorldCup2022 : public WorldCup {
         std::vector<std::shared_ptr<Player>> players;
         std::shared_ptr<ScoreBoard> scoreboard;
         std::vector<std::shared_ptr<Die>> dices;
+        int bankruptNumber = 0;
         Board board;
     public:
         WorldCup2022():
@@ -326,27 +332,38 @@ class WorldCup2022 : public WorldCup {
                 scoreboard->onRound(roundNo);
                 for (std::shared_ptr<Player> player : players) {
                     //czeka
-                    if (player.get()->skipsTurn()) {
-                        player.get()->waitOneTurn();
-                    } else if (!player.get()->getIsBankrupt()) {
+                    if (player->skipsTurn()) {
+                        player->waitOneTurn();
+                    } else if (!player->getIsBankrupt()) {
                         //gra
                         unsigned int roll = 0;
                         for (const std::shared_ptr<Die>& die: dices) {
                             roll += die->roll();
                         }
                         for (size_t i = 1; i <= roll - 1; i++) {
-                            player.get()->moveOneField(board.size());
-                            board[player.get()->getCurrField()]->passingAction(player.get());
-                            if (player.get()->getIsBankrupt()) {
+                            player->moveOneField(board.size());
+                            board[player->getCurrField()]->passingAction(player.get());
+                            if (player->getIsBankrupt()) {
+                                ++bankruptNumber;
+                                player->writeScore(scoreboard, board[player->getCurrField()]->getName());
                                 break;
                             }
                         }
-                        if (!player.get()->getIsBankrupt()) {
-                            player.get()->moveOneField(board.size());
-                            board[player.get()->getCurrField()]->landingAction(player.get());
+                        if (!player->getIsBankrupt()) {
+                            player->moveOneField(board.size());
+                            board[player->getCurrField()]->landingAction(player.get());
                         }
                     }
-                    player.get()->writeScore(scoreboard, board[player.get()->getCurrField()]->getName());
+                    if (!player->getIsBankrupt())
+                        player->writeScore(scoreboard, board[player->getCurrField()]->getName());
+
+                    if (bankruptNumber == players.size() - 1) {
+                        for (std::shared_ptr<Player> p : players) {
+                            if (!p->getIsBankrupt())
+                                scoreboard->onWin(p->getName());
+                        }
+                        return;
+                    }
                 }
             }
         }
